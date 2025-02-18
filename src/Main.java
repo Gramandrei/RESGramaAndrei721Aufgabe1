6aa4cef63b6f448f4ee63c7c2a075ab62e1645cf
Gramandrei <andrei.gabriel.grama@stud.ubbcluj.ro>
2025-02-18 10:54:23 +0200

    added Id, Held etc.

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,27 @@ import java.util.stream.Collectors;
 
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static class Event{
+
+        private int Id;
+        private String Held;
+        private String Antagonist;
+        private String Konfrontationstyp;
+        private String Ort;
+        private LocalDate Datum;
+        private double GlobalerEinfluss;
+
+
+//        - Id – Eindeutige Identifikationsnummer der Konfrontation
+//        (int)
+//        - Held – Name des Haupthelden (String)
+//        - Antagonist – Name des Hauptgegners (String)
+//        - Konfrontationstyp – Kategorie des Ereignisses (Enum:
+//        Individuell, Team, Galaktisch, Multiversal)
+//        - Ort – Ort, an dem die Konfrontation stattfand (String)
+//        - Datum – Datum des Ereignisses (LocalDate)
+//
 
 
     }
